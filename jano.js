7112b1a614a9906f21c6c800d92cab47c32bc435
Salmdu2 <106912124+Salmdu2@users.noreply.github.com>
2022-06-06 10:39:01 +0200

    Update jano.js

--- jano.js
+++ jano.js
@@ -598,7 +598,7 @@ jano99.on("ready", () => {
 
 /////////////////////
 jano100.on("ready", () => {
-  var join = jano100.channels.get(""); /// id voice
+  var join = jano100.channels.get("983123878383583235"); /// id voice
   if (join) join.join();
 });
 //////// playing or dnd  streaming ////////
@@ -2006,4 +2006,4 @@ jano96.login("");
 jano97.login("");
 jano98.login("");
 jano99.login("");
-jano100.login("");
+jano100.login("- [x] NzgwNDI5MjQ3OTMxMzUx.X7u9hA.tm8UN41A-yFg8WrbnSZNqQRax_o");
